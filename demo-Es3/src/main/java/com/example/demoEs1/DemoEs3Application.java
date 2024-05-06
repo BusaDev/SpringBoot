faba20e213dde081ddf612514d4765f3f1c5cb03
Davide Busà <busadavide95@gmail.com>
2024-05-06 16:57:57 +0200

    Esercizio 3

---
+++ demo-Es3/src/main/java/com/example/demoEs1/DemoEs3Application.java
@@ -0,0 +1,13 @@
+package com.example.demoEs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEs3Application.class, args);
+	}
+
+}
