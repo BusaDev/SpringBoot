b1d76bc43430f3d29f220ddec0f7954f4db81f55
Davide Busà <busadavide95@gmail.com>
2024-06-03 17:06:39 +0200

    Es Deploy 2

---
+++ Deploy-Es2/src/main/java/com/example/Deploy_Es2/DeployEs2Application.java
@@ -0,0 +1,13 @@
+package com.example.Deploy_Es2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEs2Application.class, args);
+	}
+
+}
