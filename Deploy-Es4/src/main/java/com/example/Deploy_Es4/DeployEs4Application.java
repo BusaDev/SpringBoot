e6cafe47041f06ddd720c9b97ec574f00044e1f1
Davide Busà <busadavide95@gmail.com>
2024-06-03 17:25:24 +0200

    Es Deploy 4

---
+++ Deploy-Es4/src/main/java/com/example/Deploy_Es4/DeployEs4Application.java
@@ -0,0 +1,13 @@
+package com.example.Deploy_Es4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEs4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEs4Application.class, args);
+	}
+
+}
---
+++ Deploy-Es4/src/test/java/com/example/Deploy_Es4/DeployEs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Deploy_Es4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEs4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
